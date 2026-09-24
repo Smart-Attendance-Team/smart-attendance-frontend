88dc878b59dbec9499ce8a9c749479d70824e867
NOUR Mahmoud <Nourmahmoudahmed2005@gmail.com>
2026-09-24 18:14:50 +0300

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.168.43.138:3000",
+  baseURL: "https://smart-attendance-backend-production-affb.up.railway.app",
 });
 
 api.interceptors.request.use((config) => {
